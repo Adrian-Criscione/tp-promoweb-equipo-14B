c29ade5dbdec1af89fd62f9012b15ab556abd263
Nazacodexy <nazareno.de@alumnos.frgp.utn.edu.ar>
2024-10-05 21:01:25 -0300

    Se agrego la clase Cliente y ClienteNegocio

---
+++ TPWeb_equipo14B/dominio/Cliente.cs
@@ -0,0 +1,30 @@
+﻿namespace dominio
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Documento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public int CP { get; set; }
+
+        public Cliente(int id, string documento, string nombre, string apellido, string email, string direccion, string ciudad, int cp)
+        {
+            Id = id;
+            Documento = documento;
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+            Direccion = direccion;
+            Ciudad = ciudad;
+            CP = cp;
+        }
+
+        public Cliente()
+        {
+        }
+    }
+}
